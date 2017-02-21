a7b80f66d6d3d96a9802d94383ba09c537b87780
hegdeashwin <ashwin.hegde3@gmail.com>
2017-02-22 00:28:35 +0530

    Update select queries

--- SQL/02_curd_tables/03_select_table.sql
+++ SQL/02_curd_tables/03_select_table.sql
@@ -1,5 +1,29 @@
+/**
+ * Basic select
+ */
 SELECT * FROM employees;
 
 SELECT name FROM employees;
 
 SELECT eid, name, department FROM employees;
+
+/**
+ * Where clause
+ */
+SELECT * FROM employees WHERE department = 'Engineering';
+
+SELECT * FROM employees WHERE eid BETWEEN 3 AND 5;
+
+/**
+ * Like clause
+ */
+SELECT * FROM employees WHERE department LIKE 'Eng%';
+
+SELECT * FROM employees WHERE department NOT LIKE 'Eng%';
+
+SELECT * FROM employees WHERE department LIKE '%neer%';
+
+/**
+ * In clause
+ */
+SELECT * FROM employees WHERE department IN ('UI', 'Engineering');
